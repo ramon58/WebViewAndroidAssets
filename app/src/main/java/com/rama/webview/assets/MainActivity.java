bc17771c3609260860454d4cdbb8c44037e1b382
ramon58 <ramoncarrion@gmail.com>
2016-12-11 19:05:04 +0100

    Project

---
+++ app/src/main/java/com/rama/webview/assets/MainActivity.java
@@ -0,0 +1,37 @@
+package com.rama.webview.assets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        wb = (WebView) findViewById(R.id.webView);
+
+        //String url="http://www.regilog.com";
+        String url= "file:///android_asset/ejemplo.html";
+
+        wb.getSettings().setJavaScriptEnabled(true);
+        wb.getSettings().setBuiltInZoomControls(true);
+        wb.loadUrl(url);
+        wb.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading (WebView view, WebResourceRequest request){
+
+                return false;
+
+            }
+        });
+    }
+}
